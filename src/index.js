74ccbd14b3ede0c3b16b4def2bc8c3921bb58742
Sharad Mishra <sharadrx9@gmail.com>
2025-04-02 17:53:38 +0000

    Some minor changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express=require('express');
 
-const {PORT}=require('./config/serverConfig');
+const { PORT } = require('./config/serverConfig');
+const bodyParser = require('body-parser');
 
 const setupAndStartServer = async()=>{
+
+
     //creating the express object
     const app=express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
     app.listen(PORT,()=>{
         console.log(`Server is running on port ${PORT}`);
         
