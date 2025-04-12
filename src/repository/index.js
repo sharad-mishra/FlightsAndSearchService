75c562ba64ee60d8cc41baa0afce002326434064
Sharad Mishra <sharadrx9@gmail.com>
2025-04-12 12:38:29 +0000

    Added common CRUD for repetitive operations

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,6 +1,7 @@
-
-module.exports={
+module.exports = {
     CityRepository: require('./city-repository'),
     FlightRepository: require('./flight-repository'),
-    AirplaneRepository: require('./airplane-repository'),
+    AirplaneRepository: require('./airplane-repository'), // Fixed spelling
+    AirportRepository: require('./airport-repository'),
+    CrudRepository: require('./crud-repository')
 }
